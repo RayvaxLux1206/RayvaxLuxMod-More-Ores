632597ec3ee69bf9c6e5ad44474795dd7b0031b7
RayvaxLux1206 <98554590+RayvaxLux1206@users.noreply.github.com>
2025-09-09 13:18:33 -0400

    More Ores 1.1.0 update

---
+++ src/main/java/rvl/more_ores/item/special_item/ToxicMaterialItem.java
@@ -0,0 +1,46 @@
+package rvl.more_ores.item.special_item;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ToxicMaterialItem extends Item {
+    private final int damageRangeOnHealh;
+    private final StatusEffect targetEffect;
+    public ToxicMaterialItem(Settings settings, StatusEffect targetEffect, int damageRangeOnHealh) {
+        super(settings);
+        this.damageRangeOnHealh = damageRangeOnHealh;
+        this.targetEffect = targetEffect;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(itemstack, world, entity, slot, selected);
+        if (entity instanceof PlayerEntity player) {
+            if (player.getMainHandStack().isOf(this) || player.getOffHandStack().isOf(this)) {
+                if (player.getHealth() > player.getMaxHealth() / this.damageRangeOnHealh) {
+                    player.setHealth(player.getMaxHealth() - (player.getMaxHealth() / this.damageRangeOnHealh));
+                }
+                player.addStatusEffect(new StatusEffectInstance(this.targetEffect, 10, 1, false, false));
+            }
+        }
+
+    }
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (!target.getWorld().isClient()) {
+            target.addStatusEffect(new StatusEffectInstance(this.targetEffect, 100, 1));
+        }
+        return true;
+    }
+
+
+}
+
---
+++ src/main/java/rvl/more_ores/item/special_item/ToxicPickaxeItem.java
@@ -0,0 +1,26 @@
+package rvl.more_ores.item.special_item;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import rvl.more_ores.item.Fixer.FixPickaxeItem;
+import rvl.more_ores.item.Fixer.FixToolMaterial;
+
+public class ToxicPickaxeItem extends FixPickaxeItem {
+    private final StatusEffect hitEffect;
+    public ToxicPickaxeItem(FixToolMaterial material, float attackDamage, float attackSpeed, Settings settings, StatusEffect hitEffect) {
+        super(material, attackDamage, attackSpeed, settings);
+        this.hitEffect = hitEffect;
+    }
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        super.postHit(stack, target, attacker);
+        if (!target.getWorld().isClient()) {
+            target.addStatusEffect(new StatusEffectInstance(this.hitEffect, 100, 1));
+
+        }
+
+        return true;
+    }
+}
